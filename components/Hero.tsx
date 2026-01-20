dcca72f825b575555456095af3c264140bb3d7e7
katsura919 <katsuragik919@gmail.com>
2026-01-21 02:46:27 +0800

    adjust header

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
     return (
         <section
             id="home"
-            className="relative min-h-screen flex items-center justify-center overflow-hidden"
+            className="relative min-h-screen flex items-center justify-center overflow-hidden pt-24 md:pt-0"
         >
             {/* Background Effects */}
             <div className="absolute inset-0 bg-white" />
